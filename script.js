e2cdadc0aeea64d48415bb5ac9736968d7045086
QWR3 <anton004@protonmail.com>
2021-06-05 22:40:31 +0300

    1

---
+++ script.js
@@ -0,0 +1,165 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// let hello = 'hello';
+// let owu = 'owu';
+// let com = 'com';
+// let ua = 'ua';
+// let one = 1;
+// let ten = 10;
+// let minus = -999;
+// let one_two_three = 123;
+// let pi = 3.14;
+// let two_dot_seven = 2.7;
+// let sixteen = 16;
+// let tru = true;
+// let fals = false;
+
+//   Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(hello);
+// alert(hello);
+// document.write(hello);
+// console.log(owu);
+// alert(owu);
+// document.write(owu);
+// console.log(com);
+// alert(com);
+// document.write(com);
+// console.log(ua);
+// alert(ua);
+// document.write(ua);
+// console.log(one);
+// alert(one);
+// document.write(one);
+// console.log(ten);
+// alert(ten);
+// document.write(ten);
+// console.log(minus);
+// alert(minus);
+// document.write(minus);
+// console.log(one_two_three);
+// alert(one_two_three);
+// document.write(one_two_three);
+// console.log(pi);
+// alert(pi);
+// document.write(pi);
+// console.log(two_dot_seven);
+// alert(two_dot_seven);
+// document.write(two_dot_seven);
+// console.log(sixteen);
+// alert(sixteen);
+// document.write(sixteen);
+// console.log(tru);
+// alert(tru);
+// document.write(tru);
+// console.log(fals);
+// alert(fals);
+// document.write(fals);
+
+// - Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
+// hello = Math.round(Math.random() * 100);
+// owu = Math.round(Math.random() * 100);
+// com = Math.round(Math.random() * 100);
+// ua = Math.round(Math.random() * 100);
+// one = Math.round(Math.random() * 100);
+// ten = Math.round(Math.random() * 100);
+// minus = Math.round(Math.random() * 100);
+// one_two_three = Math.round(Math.random() * 100);
+// pi = Math.round(Math.random() * 100);
+// two_dot_seven = Math.round(Math.random() * 100);
+// sixteen = Math.round(Math.random() * 100);
+// tru = Math.round(Math.random() * 100);
+// fals = Math.round(Math.random() * 100);
+
+//   Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(hello);
+// alert(hello);
+// document.write(hello);
+// console.log(owu);
+// alert(owu);
+// document.write(owu);
+// console.log(com);
+// alert(com);
+// document.write(com);
+// console.log(ua);
+// alert(ua);
+// document.write(ua);
+// console.log(one);
+// alert(one);
+// document.write(one);
+// console.log(ten);
+// alert(ten);
+// document.write(ten);
+// console.log(minus);
+// alert(minus);
+// document.write(minus);
+// console.log(one_two_three);
+// alert(one_two_three);
+// document.write(one_two_three);
+// console.log(pi);
+// alert(pi);
+// document.write(pi);
+// console.log(two_dot_seven);
+// alert(two_dot_seven);
+// document.write(two_dot_seven);
+// console.log(sixteen);
+// alert(sixteen);
+// document.write(sixteen);
+// console.log(tru);
+// alert(tru);
+// document.write(tru);
+// console.log(fals);
+// alert(fals);
+// document.write(fals);
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. Зконкатенувати їх в одну змінну person.
+// let firstName_ = 'Anton';
+// let middleName_ = 'middleName of Anton';
+// let lastName_ = 'lastName of Anton';
+//
+// let person = {
+//   firstName: firstName_,
+//   middleName: middleName_,
+//   lastName: lastName_,
+// }
+// console.log(person);
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+// let name = prompt(`your name`);
+// let middleName = prompt(`your middle name`);
+// let year = +prompt(`how old are u?`);
+
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+// document.write(`Вітаю  ${name}. Тобі ${year} років`);
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(a + "-" + (typeof a));
+// console.log(b + "-" + (typeof b));
+// console.log(c + "-" + (typeof c));
+
+// - Поставет відповідний оператор в виразах що б вийшов відповідний результат.
+// В однакових виразаї не використовувати однакові оператори!!!
+  console.log(5 < 6);
+  // -> true
+  console.log(5 > 6);
+  // -> false
+  console.log(5 == 6);
+  // -> false
+  console.log(5 === 6);
+   // -> false
+  console.log(10 == 10);
+  // -> true
+  console.log(10 === 10);
+  // -> true
+  console.log(10 !== 10);
+  // -> false
+  console.log(10 != 10);
+  // -> false
+  console.log(10 > 10);
+  // -> false
+  console.log(123 === '123');
+   // -> false
+  console.log(123 == '123');
+  // -> true
+//
